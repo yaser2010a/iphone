fd5aba6bbc00c338397d3fdfaffe7d10e7acf94b
yaser 2010 <yaser@example.com>
2026-08-16 21:25:55 +0300

    أول رفع

---
+++ script.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.iphone');
+let page = document.querySelector('.page');
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    page.style.background = color;
+}
